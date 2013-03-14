449b73d5be5bd39f3ebfb37585f5bd1d25ec36e5
yellhb <yellhb@gmail.com>
2013-03-14 13:56:37 +0000

    完成部分页面切换，需要添加ListAdapter

--- gen/com/artifex/mupdf/R.java
+++ gen/com/artifex/mupdf/R.java
@@ -65,9 +65,10 @@ public final class R {
         public static final int buttons=0x7f030000;
         public static final int item=0x7f030001;
         public static final int main=0x7f030002;
-        public static final int outline_entry=0x7f030003;
-        public static final int picker_entry=0x7f030004;
-        public static final int titlebar=0x7f030005;
+        public static final int main_list=0x7f030003;
+        public static final int outline_entry=0x7f030004;
+        public static final int picker_entry=0x7f030005;
+        public static final int titlebar=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
